d1617e387cb46b5eed845a3335122eab137cdced
Byte <bytequivar@gmail.com>
2024-07-17 20:32:56 +0800

    Initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-black'>
+        <ul>
+            <li>
+                <a>About</a>
+            </li>
+            <li>
+                <a>Contact</a>
+            </li>
+            <li>
+                <a>Project</a>
+            </li>
+            <li>
+                <a>Blog</a>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
